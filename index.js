382147910bade3d3c46bb02215fbc2925bc6df19
Orlando Axalco <147562150+Oaxalco@users.noreply.github.com>
2024-09-12 21:16:29 -0700

    commit

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ function addTask(){
  deleteBtn.textContent= 'Delete';
  deleteBtn.addEventListener('click', deleteTask);
  li.appendChild(deleteBtn)
+
+ saveTaskstoLocalStorage();
+ loadTasksFromLocalStorage();
 }
 
 function completeTask(event){
@@ -25,4 +28,31 @@ function completeTask(event){
 function deleteTask(event){
     const task = event.target.parentElement;
     taskList.removeChild(task);
+}
+
+function saveTaskstoLocalStorage(){
+    const tasks= [];
+    const taskItems= taskList.getElementsByTagName('li');
+    
+    for (let i = 0; i <taskItems.length; i++){
+        tasks.push(taskItems[i].textContent);
+    }
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasksFromLocalStorage(){
+    const tasks= JSON.parse(localStorage.getItem('tasks'));
+    if(tasks){
+        tasks.forEach(taskText =>{
+            const li = document.createElement('li');
+            li.textContent= taskText;
+            taskList.appendChild(li);
+            li.addEventListener('click', completeTask);
+
+            const deleteBtn= document.createElement('button');
+            deleteBtn.textContent= 'Delete';
+            deleteBtn.addEventListener('click', deleteTask);
+            li.appendChild(deleteBtn);
+        });
+    }
 }
